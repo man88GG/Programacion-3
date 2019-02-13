0269d7a42a8c44b07bfd87375d9b5c4337d33040
man88GG <opfuga@gmail.com>
2019-02-12 18:16:34 -0600

    Cambios Tabla 2

--- src/Inicio.java
+++ src/Inicio.java
@@ -31,7 +31,7 @@ public class Inicio extends javax.swing.JFrame {
     String Encab [] = {"Nombre","Salario Base","Total Deducciones","Total Percepciones","ISR","Sueldo Liquido","Num Dept."};
     
     //vector para 2da tabla
-  String Encabezado [] = {"Numero Dept.","1","2","3","4","5"};
+    String Encabezado [] = {"Numero Dept.","1","2","3","4","5"};
     //Declaracion de variable random para el sueldo y los totales
     Random NumR = new Random();
     //Declaracion para ingresar datos en la Tabla
@@ -111,16 +111,7 @@ public void Ingreso_Tabla(){
     String Datos [] = {Nombre,Salario_Base,Tot_Deducc,Tot_Percept,Isr,Sal_Liq,Depart};
     //Agregando el vector a la fila de la tabla
     Modelo.addRow(Datos);
-   /*
-    switch(Dept){
-        case 1:{t1+=Sueldo_Liquido;}break;
-        case 2:{t2+=Sueldo_Liquido;}break;
-        case 3:{t3+=Sueldo_Liquido;}break;
-        case 4:{t4+=Sueldo_Liquido;}break;
-        case 5:{t5+=Sueldo_Liquido;}break;
  
-    }
-    */
     
 }
     
@@ -143,6 +134,10 @@ public void Ingreso_Tabla(){
         agregar[5]=String.valueOf(t5);
         
         ModeloVect.addRow(agregar);
+        
+        
+        
+        
  }
     /**
      * This method is called from within the constructor to initialize the form.
@@ -159,11 +154,14 @@ public void Ingreso_Tabla(){
         txtNom_Emp = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblDatos = new javax.swing.JTable();
+        jPanel1 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         tblData = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        Pnl_Ingreso_Planilla.setBackground(new java.awt.Color(255, 255, 255));
 
         btnIngresar.setText("Ingresar Dato a Planilla");
         btnIngresar.addActionListener(new java.awt.event.ActionListener() {
@@ -201,16 +199,17 @@ public void Ingreso_Tabla(){
                 .addGroup(Pnl_Ingreso_PlanillaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(Pnl_Ingreso_PlanillaLayout.createSequentialGroup()
                         .addContainerGap()
-                        .addGroup(Pnl_Ingreso_PlanillaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(Pnl_Ingreso_PlanillaLayout.createSequentialGroup()
-                                .addComponent(lblNom_Emp)
-                                .addGap(45, 45, 45)
-                                .addComponent(txtNom_Emp, javax.swing.GroupLayout.PREFERRED_SIZE, 301, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 863, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(lblNom_Emp)
+                        .addGap(45, 45, 45)
+                        .addComponent(txtNom_Emp, javax.swing.GroupLayout.PREFERRED_SIZE, 301, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(Pnl_Ingreso_PlanillaLayout.createSequentialGroup()
-                        .addGap(370, 370, 370)
-                        .addComponent(btnIngresar)))
-                .addContainerGap(46, Short.MAX_VALUE))
+                        .addGap(23, 23, 23)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 863, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(35, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, Pnl_Ingreso_PlanillaLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(btnIngresar)
+                .addGap(363, 363, 363))
         );
         Pnl_Ingreso_PlanillaLayout.setVerticalGroup(
             Pnl_Ingreso_PlanillaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -220,11 +219,13 @@ public void Ingreso_Tabla(){
                     .addComponent(txtNom_Emp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblNom_Emp))
                 .addGap(34, 34, 34)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
-                .addGap(40, 40, 40)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+                .addGap(29, 29, 29)
                 .addComponent(btnIngresar, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(24, 24, 24))
         );
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         tblData.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -246,6 +247,29 @@ public void Ingreso_Tabla(){
 
         jLabel1.setText("Tabla de Totales Salario Liquido por Dept");
 
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(402, 402, 402)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(118, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 695, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(77, 77, 77))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 20, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -254,25 +278,19 @@ public void Ingreso_Tabla(){
                 .addContainerGap()
                 .addComponent(Pnl_Ingreso_Planilla, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(386, 386, 386)
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane2)
-                .addGap(28, 28, 28))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(Pnl_Ingreso_Planilla, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(27, 27, 27)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(28, Short.MAX_VALUE))
         );
 
         pack();
@@ -340,19 +358,14 @@ public void Ingreso_Tabla(){
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Pnl_Ingreso_Planilla;
-    private javax.swing.JPanel Pnl_Ingreso_Planilla1;
     private javax.swing.JButton btnIngresar;
-    private javax.swing.JButton btnIngresar1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JLabel lblNom_Emp;
-    private javax.swing.JLabel lblNom_Emp1;
     private javax.swing.JTable tblData;
     private javax.swing.JTable tblDatos;
-    private javax.swing.JTable tblDatos1;
     private javax.swing.JTextField txtNom_Emp;
-    private javax.swing.JTextField txtNom_Emp1;
     // End of variables declaration//GEN-END:variables
 }
